3c778274eebe12d431dfadd2b103a6cc6e8012d6
naeemalomari <naeemalomari96@gmail.com>
2021-10-06 15:02:50 +0300

    Test Challenge 5

--- java/ChallengeFive/app/src/test/java/ChallengeFive/AppTest.java
+++ java/ChallengeFive/app/src/test/java/ChallengeFive/AppTest.java
@@ -7,15 +7,20 @@ import ChallengeFive.stucture.LinkedList;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 class AppTest {
-  //    @Test void appHasAGreeting() {
-//        LinkedList classUnderTest = new LinkedList();
-//    assertEquals();
-//    }
-//    @Test void appHasAGreeting() {
-//        LinkedList classUnderTest = new LinkedList();
-//    assertEquals();
-//    }
-//    @Test void appHasAGreeting(){
-//      assertTrue(appHasAGreeting(), "NA");
-//    }
+      @Test void appHasAGreeting() {
+
+        LinkedList classUnderTest = new LinkedList();
+        classUnderTest.insert("E");
+        classUnderTest.insert("N");
+        classUnderTest.insert("G");
+        classUnderTest.insert("N");
+        classUnderTest.insert("A");
+        classUnderTest.insert("I");
+        classUnderTest.insert("M.");
+        assertEquals(" { E }  -------->  { N }  -------->  { G }  -------->  { N }  -------->  { A }  -------->  { I }  -------->  { M. }  -------->  NULL ", classUnderTest.toString());
+        assertTrue(classUnderTest.include("E"));
+
+      }
+
+
 }
