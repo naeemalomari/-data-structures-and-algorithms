62d831704588205ca1d2b29d0cc68937d2f0e1ca
naeemalomari <naeemalomari96@gmail.com>
2021-10-07 20:43:25 +0300

    cc10

--- java/ChallengeFive/app/src/main/java/ChallengeFive/App.java
+++ java/ChallengeFive/app/src/main/java/ChallengeFive/App.java
@@ -3,10 +3,13 @@
  */
 package ChallengeFive;
 import ChallengeFive.LinkedList.LinkedList;
+import ChallengeFive.Queue.Queue;
+import ChallengeFive.stack.Stack;
+
 public class App {
   public static void main(String[] args) {
     LinkedList();
-
+    stack();
   }
   public static void LinkedList(){
     LinkedList linkedList = new LinkedList();
@@ -20,6 +23,41 @@ public class App {
     System.out.println(linkedList);
     System.out.println(linkedList.include("N"));
     System.out.println(linkedList.include("P"));
+
+  }
+  public static void stack(){
+    Stack stack = new Stack();
+    stack.push("N");
+    stack.push("A");
+    stack.push("E");
+    stack.push("E");
+    stack.push("M");
+    stack.push("1");
+    stack.push("2");
+    stack.push("3");
+    stack.push("4");
+
+    stack.pop();
+    stack.pop();
+    stack.pop();
+    stack.pop();
+
+    stack.peek();
+  }
+  public static void queue(){
+    Queue queue =new Queue();
+
+
+    queue.enqueue("N");
+    queue.enqueue("A");
+    queue.enqueue("E");
+    queue.enqueue("E");
+    queue.enqueue("M");
+    queue.enqueue("1");
+    queue.enqueue("2");
+    queue.enqueue("3");
+    queue.enqueue("4");
+
 
   }
 }
